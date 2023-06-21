3a2b3f442fe1bba9fa699920cf814939bad6d5b9
SpAwNnScode <137146049+SpAwNnScode@users.noreply.github.com>
2023-06-21 05:29:39 +0200

    Time: 21 ms (32.01%), Space: 58 MB (14.79%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++)
+            {
+                    if(nums[i]==nums[i+1])
+                    {
+                        return true;
+                    }
+
+                }
+
+   return false; }
+}
