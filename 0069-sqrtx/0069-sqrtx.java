4a0f805d3b143576c351c2dd1b864adde2518218
SpAwNnScode <137146049+SpAwNnScode@users.noreply.github.com>
2023-06-29 07:46:45 +0200

    Time: 1 ms (100.00%), Space: 39.7 MB (72.62%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int mySqrt(int x) {
+       int b=0;
+        if(x<0)
+        {
+            System.out.println("its negativ");
+        }
+        else
+        {
+            b=(int)Math.sqrt(x);
+        }
+
+
+
+    return b;}
+}
