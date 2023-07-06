67f14bdddc1326c8d7a9fb5f0034ad2156ee96d3
SpAwNnScode <137146049+SpAwNnScode@users.noreply.github.com>
2023-07-06 19:51:02 +0200

    Time: 4 ms (85.78%), Space: 43.8 MB (34.68%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static boolean isAnagram(String s, String t) {
+
+            char[] S = s.toCharArray();
+            char[] T = t.toCharArray();
+
+            Arrays.sort(S);
+            Arrays.sort(T);
+
+            return Arrays.equals(S, T);
+
+    }
+}
